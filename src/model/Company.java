51e15b96d8dfcda9f12b058451b34a5c724c1a42
RodrigoMelo10 <digoliveira10@yahoo.com.br>
2020-05-16 23:17:49 +0100

    creating Company class

--- src/model/Company.java
+++ src/model/Company.java
@@ -1,5 +1,26 @@
 package model;
 
-public class Company {
+import patters.Observer;
 
-}
+public class Company implements Observer{
+
+    private long id;
+    private String name;
+    private Integer shares;
+    private Integer sharePrice;
+    private Integer capital;
+    private Integer sharedSelleds;
+    private boolean outOfShares;
+    
+    public Company(long id) {
+        this.id = id;
+    }
+// passing attributes
+    public Company(long id, String name, Integer shares, Integer sharePrice, boolean outOfShares) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.shares = shares;
+        this.sharePrice = sharePrice;
+        this.outOfShares = outOfShares;
+    }
